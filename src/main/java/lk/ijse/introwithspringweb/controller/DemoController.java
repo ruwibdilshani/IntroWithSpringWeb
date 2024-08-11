08a496f50ac53d38c723a805aa33f1e6f5c24dd2
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-08-11 15:17:20 +0530

    Test mapping spec with pathVariable

--- src/main/java/lk/ijse/introwithspringweb/controller/DemoController.java
+++ src/main/java/lk/ijse/introwithspringweb/controller/DemoController.java
@@ -1,6 +1,7 @@
 package lk.ijse.introwithspringweb.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +16,19 @@ public class DemoController {
 
 //    @GetMapping(value = "/healt?")
 
-    @GetMapping(value = "/health/**")//ona taramak path danna plwn
+    //@GetMapping(value = "/health/**")//ona taramak path danna plwn
 
-    public String healthCheck(){
-        return "DemoController run perfectly";
+//    @GetMapping(value = "/pattern/{name}")
+//    public String pathVariable(@PathVariable ("name")String name){
+//        return "DemoController run perfectly with :"+name;
+//
+//}
+
+    @GetMapping(value = "/health/{id:C\\d{3}}") //regex
+    public String healthCheck(@PathVariable ("id")String id){
+        return "DemoController run perfectly with :"+id;
     }
+
 
     @GetMapping
     public String otherTest(){
